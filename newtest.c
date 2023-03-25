59b974dcc3a84878d89861e7800c34bbb66b9ac0
Lartu <lartu@lartu.net>
2023-03-25 07:07:24 -0300

    Sprite test

--- newtest.c
+++ newtest.c
@@ -1,7 +1,5 @@
 #include "lamadil.h"
 
-uint32 state = 0;
-
 void start()
 {
 }
@@ -18,39 +16,56 @@ uint32 speed_x3 = 3;
 uint32 speed_y3 = -3;
 uint32 x3 = 50;
 uint32 y3 = 50;
+
 uint32 color = 0;
 uint32 color_speed = 1;
+uint32 color2 = 10;
+uint32 color_speed2 = 1;
+uint32 color3 = 20;
+uint32 color_speed3 = 1;
+
+uint32 ldply = 0;
+uint32 ldplspeed = 1;
 
 void update()
 {
-    fill_screen(create_color(color, color, color));
+    fill_screen(create_color(color, color2, color3));
+
+    for(uint32 iy = 0; iy < 64; ++iy)
+    {
+        for(uint32 ix = 0; ix < 64; ++ix)
+        {
+            // TODO: esto es re optimizable haciendo una
+            // funcion de sprites custom que memcopyee
+            set_pixel(ix, iy + ldply, bitmap[ix + iy*64]);
+        }
+    }
+
+    ldply += ldplspeed;
+    if(ldply == 0 || ldply == 160-64) ldplspeed = -ldplspeed;
+    
     color += color_speed;
     if(color == 0 || color == 31) color_speed = -color_speed;
+    color2 += color_speed2;
+    if(color2 == 0 || color2 == 31) color_speed2 = -color_speed2;
+    color3 += color_speed3;
+    if(color3 == 0 || color3 == 31) color_speed3 = -color_speed3;
 
-    set_pixel(x+1, y, create_color(31-color, 31-color, 31-color));
-    set_pixel(x, y+1, create_color(31-color, 31-color, 31-color));
-    set_pixel(x+2, y+1, create_color(31-color, 31-color, 31-color));
-    set_pixel(x+1, y+2, create_color(31-color, 31-color, 31-color));
-    set_pixel(x+1, y+1, create_color(31, 0, 0));
-
-    set_pixel(x2+1, y2, create_color(31-color, 31-color, 31-color));
-    set_pixel(x2,   y2+1, create_color(31-color, 31-color, 31-color));
-    set_pixel(x2+2, y2+1, create_color(31-color, 31-color, 31-color));
-    set_pixel(x2+1, y2+2, create_color(31-color, 31-color, 31-color));
-    set_pixel(x2+1, y2+1, create_color(0, 31, 0));
-
-    set_pixel(x3+1, y3, create_color(31-color, 31-color, 31-color));
-    set_pixel(x3,   y3+1, create_color(31-color, 31-color, 31-color));
-    set_pixel(x3+2, y3+1, create_color(31-color, 31-color, 31-color));
-    set_pixel(x3+1, y3+2, create_color(31-color, 31-color, 31-color));
-    set_pixel(x3+1, y3+1, create_color(0, 0, 31));
-
-    for(uint32 iy = 0; iy < 16; ++iy)
-    {
-        for(uint32 ix = 0; ix < 16; ++ix)
-        {
-            Color color = create_color(bitmap[ix + iy*16], bitmap[ix + iy*16], bitmap[ix + iy*16]);
-            if(color > 0) set_pixel(x+ix, y+iy, color);
+    for(int iy = 0; iy < 32; ++iy){
+        for(int ix = 0; ix < 32; ++ix){
+            set_pixel(x+ix, y+iy, create_color(31-screen_buffer[(x+ix) + (y+iy) * screen_width], 31-screen_buffer[(x+ix) + (y+iy) * screen_width], 31-screen_buffer[(x+ix) + (y+iy) * screen_width]));
+        }
+    }
+    
+    for(int iy = 0; iy < 32; ++iy){
+        for(int ix = 0; ix < 32; ++ix){
+            set_pixel(x2+ix, y2+iy, 0x1FFF-screen_buffer[(x2+ix) + (y2+iy) * screen_width]);
+        }
+    }
+    
+    for(int iy = 0; iy < 32; ++iy){
+        for(int ix = 0; ix < 32; ++ix){
+            set_pixel(x3+ix, y3+iy, create_color(screen_buffer[(x3+ix) + (y3+iy) * screen_width], screen_buffer[(x3+ix) + (y3+iy) * screen_width], screen_buffer[(x3+ix) + (y3+iy) * screen_width]));
         }
     }
 
@@ -61,11 +76,12 @@ void update()
     x3 += speed_x3;
     y3 += speed_y3;
 
-    if(x <= 10 || x >= 240-13) speed_x = -speed_x;
-    if(y <= 10 || y >= 160-13) speed_y = -speed_y;
-    if(x2 <= 10 || x2 >= 240-13) speed_x2 = -speed_x2;
-    if(y2 <= 10 || y2 >= 160-13) speed_y2 = -speed_y2;
-    if(x3 <= 10 || x3 >= 240-13) speed_x3 = -speed_x3;
-    if(y3 <= 10 || y3 >= 160-13) speed_y3 = -speed_y3;
+    if(x+speed_x <= 0 || x+speed_x >= 240-32) speed_x = -speed_x;
+    if(y+speed_y <= 0 || y+speed_y >= 160-32) speed_y = -speed_y;
+    if(x2+speed_x2 <= 0 || x2+speed_x2 >= 240-32) speed_x2 = -speed_x2;
+    if(y2+speed_y2 <= 0 || y2+speed_y2 >= 160-32) speed_y2 = -speed_y2;
+    
+    if(x3+speed_x3 <= 0 || x3+speed_x3 >= 240-32) speed_x3 = -speed_x3;
+    if(y3+speed_y3 <= 0 || y3+speed_y3 >= 160-32) speed_y3 = -speed_y3;
 
 }
